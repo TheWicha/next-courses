78efc4c08e77739d245ec41240e5aa287cdb3987
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-02-09 20:23:33 +0100

    remove conole.log

--- app/(router)/course-preview/[courseId]/page.jsx
+++ app/(router)/course-preview/[courseId]/page.jsx
@@ -5,7 +5,7 @@ import useGetLessons from "../../../../utils/useGetCourseById";
 
 const CoursePreviev = ({ params }) => {
   const chapters = useGetLessons({ data: params.courseId });
-  console.log(chapters, "chapters");
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 p-5 gap-3">
       <div className="col-span-2 bg-white p-3 rounded-xl ">
