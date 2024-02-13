a8caab7cbaa3945d9ba9464ec21795431f70970a
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-02-13 08:43:51 +0100

    add copy to courseEnrollSection

--- app/(router)/course-preview/[courseId]/[courseLesson]/CourseEnrollSection/CourseEnrollSection.tsx
+++ app/(router)/course-preview/[courseId]/[courseLesson]/CourseEnrollSection/CourseEnrollSection.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const CourseEnrollSection = () => {
-  return <div>CourseEnrollSection</div>;
+  return (
+    <div className="p-3 text-center rounded-sm bg-primary ">
+      <h2 className="text-[22px] font-bold text-white">Enroll to the course</h2>
+      <h2>Enroll now to Start Learning and Unleash Your Creativity </h2>
+    </div>
+  );
 };
 
 export default CourseEnrollSection;
