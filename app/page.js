fb8a44d12d039bd0e744b7d1bcadbee81e0fe769
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-02-13 14:18:51 +0100

    add router push when user is logged in

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
+"use client";
+import { useEffect } from "react";
 import { Button } from "../components/ui/lib/button";
-import { UserButton } from "@clerk/nextjs";
-
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    user ? router.push("dashboard") : isLoaded && router.push("courses");
+  });
+
   return (
     <div>
       <Link href="/courses">
-        {" "}
         <Button>Start</Button>
       </Link>
       <UserButton afterSignOutUrl="/sign-in" />
