e8ba248d600654f7238a97691f2ee57bddfa8178
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-02-20 21:32:38 +0100

    update video player

--- components/ui/VideoPlayer/VideoPlayer.tsx
+++ components/ui/VideoPlayer/VideoPlayer.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import { VideoPlayerType } from "./VideoPlayerTypes";
 
 const VideoPlayer: React.FC<VideoPlayerType> = ({ videoUrl }) => {
+  const handleContextMenu = (event: React.MouseEvent) => {
+    event.preventDefault();
+  };
+
   return (
     videoUrl && (
-      <video width={1000} height={250} controls className="rounded-sm">
+      <video
+        width={1000}
+        height={250}
+        controls
+        controlsList="nodownload"
+        className="rounded-sm"
+        onContextMenu={handleContextMenu}
+      >
         <source src={videoUrl} type="video/mp4" />
       </video>
     )
