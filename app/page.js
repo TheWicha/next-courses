d65218f0125a5ed458137354d23926a2c099fa06
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-02-22 19:36:37 +0100

    add landing page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Button } from "../components/ui/lib/button";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/lib/button";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
@@ -11,11 +12,373 @@ export default function Home() {
   useEffect(() => {
     user && router.push("dashboard");
   });
+
   return (
     <div>
-      <Link href="/courses">
-        <Button>Start</Button>
-      </Link>
+      <nav
+        id="header"
+        className="fixed w-full z-30 top-0 text-white bg-violet-800"
+      >
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+          <div className="pl-4 flex items-center">
+            <a
+              className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+              href="#"
+            >
+              <svg
+                className="h-8 fill-current inline"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512.005 512.005"
+              >
+                <rect
+                  fill="#2a2a31"
+                  x="16.539"
+                  y="425.626"
+                  width="479.767"
+                  height="50.502"
+                  transform="matrix(1,0,0,1,0,0)"
+                />
+                <path
+                  className="plane-take-off"
+                  d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
+                />
+              </svg>
+              LANDING
+            </a>
+          </div>
+          <div
+            className=" flex-grow flex items-center w-auto mt-2 :mt-0 bg-white bg-transparent text-black p-4 :p-0 z-20"
+            id="nav-content"
+          >
+            <div className="list-reset flex justify-end flex-1 items-center">
+              <Link href="sign-in">
+                <Button
+                  id="navAction"
+                  className="mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+                >
+                  Sign In
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+      </nav>
+
+      <div className="pt-24">
+        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center ">
+          <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+            <h1 className="my-4 text-5xl font-bold leading-tight">
+              Join Photography Academy
+            </h1>
+            <p className="leading-normal text-2xl mb-8">
+              Sub-hero message, not too long and not too short. Make it just
+              right!
+            </p>
+            <Button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+              Subscribe
+            </Button>
+          </div>
+
+          <div className="w-full flex md:w-3/5 py-6 text-center md:justify-end">
+            <img className="w-1/2 h-1/2 z-50" src="hero.svg" />
+          </div>
+        </div>
+      </div>
+
+      <section className="bg-white border-b py-8">
+        <div className="container max-w-5xl mx-auto m-8">
+          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Enhance you camera skils!
+          </h2>
+          <div className="w-full mb-4">
+            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div className="flex flex-wrap">
+            <div className="w-5/6 sm:w-1/2 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Lorem ipsum dolor sit amet
+              </h3>
+              <p className="text-gray-600 mb-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                at ipsum eu nunc commodo posuere et sit amet ligula.
+                <br />
+                <br />
+                Images from:
+                <a
+                  className="text-pink-500 underline"
+                  href="https://undraw.co/"
+                >
+                  undraw.co
+                </a>
+              </p>
+            </div>
+            <div className="w-full sm:w-1/2 p-6">
+              <Image src="cover.svg" width={500} height={400} alt="cover" />
+            </div>
+          </div>
+          <div className="flex flex-wrap flex-col-reverse sm:flex-row">
+            <div className="w-full sm:w-1/2 p-6 mt-6">
+              <Image width={400} height={500} alt="globe" src="globe.svg" />
+            </div>
+            <div className="w-full sm:w-1/2 p-6 mt-6">
+              <div className="align-middle">
+                <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                  Lorem ipsum dolor sit amet
+                </h3>
+                <p className="text-gray-600 mb-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                  <br />
+                  <br />
+                  Images from:
+                  <a
+                    className="text-pink-500 underline"
+                    href="https://undraw.co/"
+                  >
+                    undraw.co
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-white border-b py-8">
+        <div className="container mx-auto flex flex-wrap pt-4 pb-12">
+          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Title
+          </h2>
+          <div className="w-full mb-4">
+            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  xGETTING STARTED
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-start">
+                <Button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Action
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  xGETTING STARTED
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-center">
+                <Button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Action
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  xGETTING STARTED
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-end">
+                <Button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Action
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="bg-white">
+        <div className="container mx-auto px-8">
+          <div className="w-full flex flex-col md:flex-row py-6">
+            <div className="flex-1 mb-6 text-black">
+              <a
+                className="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+                href="#"
+              >
+                <svg
+                  className="h-8 fill-current inline"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512.005 512.005"
+                >
+                  <rect
+                    fill="#2a2a31"
+                    x="16.539"
+                    y="425.626"
+                    width="479.767"
+                    height="50.502"
+                    transform="matrix(1,0,0,1,0,0)"
+                  />
+                  <path
+                    className="plane-take-off"
+                    d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
+                  />
+                </svg>
+                LANDING
+              </a>
+            </div>
+            <div className="flex-1">
+              <p className="uppercase text-gray-500 md:mb-6">Links</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    FAQ
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Help
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Support
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <p className="uppercase text-gray-500 md:mb-6">Legal</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Terms
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Privacy
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <p className="uppercase text-gray-500 md:mb-6">Social</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Facebook
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Linkedin
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Twitter
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <p className="uppercase text-gray-500 md:mb-6">Company</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Official Blog
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a
+                    href="#"
+                    className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <a
+          href="https://www.freepik.com/free-photos-vectors/background"
+          className="text-gray-500"
+        >
+          Background vector created by freepik - www.freepik.com
+        </a>
+      </footer>
     </div>
   );
 }
