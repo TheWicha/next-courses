41fdd762d860cffa90b3793232ccad9f1fcdd414
Daniel Wisniewski <wicha@daniels-mbp.home>
2024-02-22 20:06:59 +0100

    add className cover

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,21 @@
+import path from "path";
+import { getHashDigest } from "loader-utils";
+
+const hashOnlyIdent = (context, _, exportName) => {
+  return getHashDigest(
+    Buffer.from(
+      `filePath:${path
+        .relative(context.rootContext, context.resourcePath)
+        .replace(/\\\\+/g, "/")}` + `#className:${exportName}`
+    ),
+    "md4",
+    "base64",
+    6
+  )
+    .replace(/[^a-zA-Z0-9-_]/g, "_")
+    .replace(/^(-?\d|--)/, "_$1");
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -9,6 +27,26 @@ const nextConfig = {
         pathname: "**",
       },
     ],
+  },
+  webpack(config, { dev }) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    if (!dev) {
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes("css-loader") &&
+            !moduleLoader.loader?.includes("postcss-loader")
+          ) {
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+          }
+        });
+      });
+    }
+
+    return config;
   },
 };
 
